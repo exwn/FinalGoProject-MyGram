8322ddb6fe1a59172305ab73d7056a63839ffc77
exwn <ixwanull.4za@outlook.com>
2022-10-18 08:21:35 +0700

    feat: add users model & validate before create

---
+++ models/users.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"github.com/asaskevich/govalidator"
+	"gorm.io/gorm"
+)
+
+type Users struct {
+	gorm.Model
+	Username string `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~Please input your username"`
+	Email    string `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Please input your email,email~Oops.. Your email is invalid. Please input your valid email"`
+	Password string `gorm:"not null" json:"password" form:"password" valid:"required~Please input your password,minstringlength(6)~Oops.. Password has to have a minimum length of 6 characters"`
+	Age      int    `gorm:"not null" json:"age" form:"age" valid:"required~Please input your age,range(8|70)~Oops.. your age not enough. Minimum age must be 8 years old"`
+}
+
+func (u *Users) BeforeCreate(tx *gorm.DB) (err error) {
+	_, errCreate := govalidator.ValidateStruct(u)
+
+	if errCreate != nil {
+		err = errCreate
+		return
+	}
+
+	err = nil
+	return
+}
